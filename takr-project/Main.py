6c240002add1f4940e95cb90a5cb9bea5a0c8802
JanoutV <JanoutV@users.noreply.github.com>
2018-10-16 12:41:32 +0200

    ahoj

--- takr-project/Main.py
+++ takr-project/Main.py
@@ -1,3 +1,4 @@
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
+print("Péťa má velkou hlavu, jak pytel od banánů")
