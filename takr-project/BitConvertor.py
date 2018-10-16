d1472c7ed6e165b80572e31f9f5a078d207b4ffa
muzos07 <muzosak@seznam.cz>
2018-10-17 01:01:24 +0200

    mala zmena test

--- takr-project/BitConvertor.py
+++ takr-project/BitConvertor.py
@@ -3,3 +3,4 @@ class BitConvertor:
     def countMessageLength(self, filePath):
         # zde bude kód pro zjištění maximální délky
         return 15 # zde bude vracen počet charakterů pro skrytou zprávu
+    #mala zmena
