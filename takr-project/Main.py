f6609a43788439cd59ecc37975301fd5de34ea09
JanoutV <JanoutV@users.noreply.github.com>
2018-10-16 12:44:51 +0200

    Revert "ahoj"
    This reverts commit 6c240002add1f4940e95cb90a5cb9bea5a0c8802.

--- takr-project/Main.py
+++ takr-project/Main.py
@@ -1,4 +1,3 @@
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
-print("Péťa má velkou hlavu, jak pytel od banánů")
