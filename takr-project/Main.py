8716e44a21072e0430ffa8036c986e58870d33b1
muzos07 <muzosak@seznam.cz>
2018-10-16 12:51:03 +0200

    Update takr-project/Main.py

--- takr-project/Main.py
+++ takr-project/Main.py
@@ -1,8 +1,5 @@
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
-print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
-print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
-print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 if true:
     print("ahoj")
 
