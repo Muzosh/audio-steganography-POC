0de9941af31817600c581917f90f5a9962689050
muzos07 <muzosak@seznam.cz>
2018-10-16 12:37:37 +0200

    b

--- takr-project/Main.py
+++ takr-project/Main.py
@@ -1,3 +1,4 @@
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
+print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
