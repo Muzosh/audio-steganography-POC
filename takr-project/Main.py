e4ed93d4d53db3965ccb4f50b29353875ad7d05c
JanoutV <JanoutV@users.noreply.github.com>
2018-10-16 12:47:02 +0200

    fail
    ahoj

--- takr-project/Main.py
+++ takr-project/Main.py
@@ -3,3 +3,6 @@ print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
 print("Ahoj Malináři Vlaďo, Slávjenko a Paťke")
+if true:
+    print("ahoj")
+
