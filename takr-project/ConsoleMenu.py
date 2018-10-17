7cf2b750fedf3d7f17d94abe95708086b212123f
muzos07 <muzosak@seznam.cz>
2018-10-17 22:04:09 +0200

    Change Main

--- takr-project/ConsoleMenu.py
+++
@@ -1,2 +0,0 @@
-class ConsoleMenu:
-    def __init__(self):
--- takr-project/Main.py
+++ takr-project/Main.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-class Main:
-
-
-=======
-from ConsoleMenu import ConsoleMenu
-
-ConsoleMenu()
->>>>>>> 5b24cffecd3e9cf23d168c087761b399c0744f2f
+ZDE NEDĚLAT ŽÁDNÉ ZMĚNY
+pokud tohle vidíš, přepni se na development nebo vytvoř novou větev
